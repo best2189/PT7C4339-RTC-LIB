8dfb4454194c3654e7bc81255475950e1204e089
best2189 <72254657+best2189@users.noreply.github.com>
2025-03-29 23:56:50 +0700

    Add files via upload
    Signed-off-by: best2189 <72254657+best2189@users.noreply.github.com>

--- examples/ShowDateTime/ShowDateTime.ino
+++ examples/ShowDateTime/ShowDateTime.ino
@@ -1,1 +1,20 @@
+#include <Wire.h>
+#include "PT7C4339.h"
 
+PT7C4339 Best_rtc;
+
+void setup(void) 
+{
+    Serial.begin(9600);
+    Best_rtc.begin();
+	Best_rtc.clearAllAlarm();
+	Best_rtc.clearAlarmFlags();
+	Best_rtc.enableOscillator();
+}
+
+void loop(void) 
+{
+    String dateTime = Best_rtc.readDateTime();  // รับค่าวันที่และเวลาเป็นสตริง
+    Serial.println(dateTime);              // พิมพ์ออกทาง Serial
+    delay(1000);
+}
