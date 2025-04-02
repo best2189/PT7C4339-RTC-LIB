0863e419c1acf1a2a8d39b8fff9427e732514486
best2189 <72254657+best2189@users.noreply.github.com>
2025-04-02 14:56:57 +0700

    Update SetDateTime.ino
    Signed-off-by: best2189 <72254657+best2189@users.noreply.github.com>

--- examples/SetDateTime/SetDateTime.ino
+++ examples/SetDateTime/SetDateTime.ino
@@ -9,6 +9,7 @@ void setup(void)
   Best_rtc.begin();
   Best_rtc.clearAllAlarm();
   Best_rtc.clearAlarmFlags();
+  Best_rtc.defaultTrickleCharger(0x00);
   Best_rtc.enableOscillator();
   //วันศุกร์ ที่ 21 มีนาคม 2025 เวลา 14:10:10 ระบบเวลา 24 ชั่วโมง 
   Best_rtc.set24hrMode(Friday,21,March,2025,14,10,10);
